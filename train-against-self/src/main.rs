b8e31a7297f45f831b1af4e8da570d29fdf24a9c
Quick Chang <quick0831@gmail.com>
2025-05-10 17:28:43 +0800

    replace verbose output

--- train-against-self/src/main.rs
+++ train-against-self/src/main.rs
@@ -6,7 +6,7 @@ use qchess_bot::{
     agent::{Agent, GameSession},
     model::{Model, ModelConfig},
 };
-use shakmaty::{Chess, Color, Outcome, Position, san::SanPlus};
+use shakmaty::{Chess, Color, Outcome, Position};
 
 fn main() {
     type MyBackend = Autodiff<Wgpu<f32, i32>>;
@@ -29,11 +29,9 @@ fn main() {
             let mut game_black: Option<GameSession<_>> = None;
             let mut white_reward;
             let mut black_reward = 0.0;
-            let mut black_move = None;
             let (white_trajectory, black_trajectory, outcome) = loop {
                 white_reward = 0.1 + -0.002 * chess.fullmoves().get() as f32; // punish for making the game long
                 let white_action = game_white.make_action();
-                let white_move = SanPlus::from_move(chess.clone(), &white_action);
                 if white_action.is_capture() {
                     // reward for capturing
                     white_reward += 0.05;
@@ -46,14 +44,6 @@ fn main() {
                     white_reward += 0.05;
                     // punish for being checked
                     black_reward -= 0.05;
-                }
-                if let Some(black_move) = black_move {
-                    println!(
-                        "{:4}. b\t{}\treward: {:+.4}",
-                        chess.fullmoves().get() - 1,
-                        black_move,
-                        black_reward,
-                    );
                 }
                 if let Some(ref mut game_black) = game_black {
                     game_black.get_feedback(chess.clone(), black_reward);
@@ -78,7 +68,6 @@ fn main() {
                     white_reward -= 0.05;
                 }
                 chess.play_unchecked(&black_action);
-                black_move = Some(SanPlus::from_move(chess.clone(), &black_action));
                 // reward for capturing
                 if black_action.is_capture() {
                     black_reward += 0.05;
@@ -89,12 +78,6 @@ fn main() {
                     // reward for checking the opposing king
                     black_reward += 0.05;
                 }
-                println!(
-                    "{:4}. w\t{}\treward: {:+.4}",
-                    chess.fullmoves().get() - 1,
-                    white_move,
-                    white_reward,
-                );
                 game_white.get_feedback(chess.clone(), white_reward);
                 if let Some(outcome) = chess.outcome() {
                     break (
@@ -112,7 +95,7 @@ fn main() {
                     );
                 }
             };
-            println!("{:?}", outcome);
+            println!("Fullmoves: {:3}, Result: {}", chess.fullmoves(), outcome);
             let (white_final_reward, black_final_reward) = match outcome {
                 Outcome::Decisive {
                     winner: Color::White,
